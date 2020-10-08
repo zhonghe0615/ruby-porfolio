e9a030eecbb4d712cafff91d28f3719484eaabf4
codecademy <ccuser@codecademy.com>
2020-10-08 04:08:02 +0000

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get "about", to: "pages#about"
   root "pages#home"
 end
